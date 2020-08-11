875ff6e3cc98f993f71b90101d44984cd764c38e
Taco <65432314+tradinglounge@users.noreply.github.com>
2020-08-11 14:11:18 -0500

    Adding DisplayElement enum.

---
+++ Sandbox.Core/DisplayElement.cs
@@ -0,0 +1,23 @@
+namespace Sandbox.Core {
+    /// <summary>
+    /// Supported types of display elements.
+    /// </summary>
+    public enum DisplayElement {
+        /// <summary>
+        /// Display as a checkbox.
+        /// </summary>
+        Checkbox,
+        /// <summary>
+        /// Display as a textbox.
+        /// </summary>
+        Textbox,
+        /// <summary>
+        /// Display as a rich textbox.
+        /// </summary>
+        RichTextbox,
+        /// <summary>
+        /// Display as a drop down list.
+        /// </summary>
+        DropdownList
+    }
+}
