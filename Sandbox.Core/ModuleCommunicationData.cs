db6f898fa47a1cfca5b28ad016839d533b596db1
Taco <65432314+tradinglounge@users.noreply.github.com>
2020-08-10 21:51:01 -0500

    Uhhhhh, new to the desktop app.
    Initial commit.

---
+++ Sandbox.Core/ModuleCommunicationData.cs
@@ -0,0 +1,21 @@
+﻿namespace Sandbox.Core {
+    public class ModuleCommunicationData {
+
+        #region Properties
+        
+        public object Data { get; set; }
+        public ModuleCommunicationType CommunicationType { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public ModuleCommunicationData(ModuleCommunicationType communicationType, object data) {
+            CommunicationType = communicationType;
+            Data = data;
+        }
+
+        #endregion
+
+    }
+}
